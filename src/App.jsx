e27e2212d62c9d71c3a579f8dd1d10c0ae6a7d44
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-06 21:38:28 +0700

    feat: add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem('token');
+    return savedCount !== null ? parseInt(savedCount) : 0
+  });
+ 
+
+  const handleClick = () => {
+    setCount((prevCount) => {
+      const newCount = prevCount + 1;
+      localStorage.setItem('token', newCount);
+      return newCount;
+    });
+  };
 
   return (
     <>
@@ -18,18 +30,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClick}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
